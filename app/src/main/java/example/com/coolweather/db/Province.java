0adf471d57d3ca97c8e0424b1b4211cfe7914579
SuperLoo <yuanjinglu1994@outlook.cm>
2018-06-19 22:08:32 +0800

    database info

---
+++ app/src/main/java/example/com/coolweather/db/Province.java
@@ -0,0 +1,43 @@
+package example.com.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by jingluyuan on 6/19/18.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private  int provinceCode;
+
+    public int getId()
+    {
+        return  id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getProvinceName()
+    {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName)
+    {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode()
+    {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode)
+    {
+        this.provinceCode = provinceCode;
+    }
+}
